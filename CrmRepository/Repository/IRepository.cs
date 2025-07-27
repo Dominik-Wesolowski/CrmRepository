1a8cdc8cc6acf1a2975b948c22024052404ae38b
Dominik-Wesolowski <dominik.wesolowski@hotmail.com>
2025-07-27 21:24:03 +0200

    Move it to CS Project

---
+++ CrmRepository/Repository/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CrmRepository.Repository
+{
+    public interface IRepository<T> where T : Entity
+    {
+        T GetById(Guid id, ColumnSet columns = null);
+        Task<T> GetByIdAsync(Guid id, ColumnSet columns = null);
+
+        IEnumerable<T> GetByConditions(IEnumerable<ConditionExpression> conditions, LogicalOperator logicalOperator = LogicalOperator.And, ColumnSet columns = null);
+        Task<IEnumerable<T>> GetByConditionsAsync(IEnumerable<ConditionExpression> conditions, LogicalOperator logicalOperator = LogicalOperator.And, ColumnSet columns = null);
+
+        IEnumerable<T> GetByFilter(FilterExpression filter, ColumnSet columns = null, int pageSize = 5000, int pageNumber = 1);
+        Task<IEnumerable<T>> GetByFilterAsync(FilterExpression filter, ColumnSet columns = null, int pageSize = 5000, int pageNumber = 1);
+
+        IEnumerable<T> GetByFetchXml(string fetchXml);
+        Task<IEnumerable<T>> GetByFetchXmlAsync(string fetchXml);
+
+        Guid Create(T entity);
+        Task<Guid> CreateAsync(T entity);
+
+        void Update(T entity);
+        Task UpdateAsync(T entity);
+
+        void Upsert(T entity);
+        Task UpsertAsync(T entity);
+
+        void Delete(Guid id);
+        Task DeleteAsync(Guid id);
+
+        bool Exists(IEnumerable<ConditionExpression> conditions);
+        Task<bool> ExistsAsync(IEnumerable<ConditionExpression> conditions);
+
+        int Count(IEnumerable<ConditionExpression> conditions);
+        Task<int> CountAsync(IEnumerable<ConditionExpression> conditions);
+
+        void Associate(Guid sourceId, string relationshipName, EntityReferenceCollection targets);
+        Task AssociateAsync(Guid sourceId, string relationshipName, EntityReferenceCollection targets);
+
+        void Disassociate(Guid sourceId, string relationshipName, EntityReferenceCollection targets);
+        Task DisassociateAsync(Guid sourceId, string relationshipName, EntityReferenceCollection targets);
+
+        OrganizationResponse Execute(OrganizationRequest request);
+        Task<OrganizationResponse> ExecuteAsync(OrganizationRequest request);
+
+        Guid CreateWithRequest(T entity, Dictionary<string, object> inputParams = null);
+        void UpdateWithRequest(T entity, Dictionary<string, object> inputParams = null);
+        void DeleteWithRequest(Guid id, Dictionary<string, object> inputParams = null);
+    }
+}
---
+++ CrmRepository/Repository/Repository.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CrmRepository.Repository
+{
+    public class CrmRepository<T> : IRepository<T> where T : Entity
+    {
+        private readonly IOrganizationService _service;
+        private readonly string _entityLogicalName;
+
+        public CrmRepository(IOrganizationService service)
+        {
+            _service = service;
+            _entityLogicalName = typeof(T).GetField("EntityLogicalName")?.GetValue(null)?.ToString()
+                ?? throw new InvalidOperationException("Missing EntityLogicalName.");
+        }
+
+        public T GetById(Guid id, ColumnSet columns = null)
+            => _service.Retrieve(_entityLogicalName, id, columns ?? new ColumnSet(true)).ToEntity<T>();
+
+        public async Task<T> GetByIdAsync(Guid id, ColumnSet columns = null)
+            => await Task.FromResult(GetById(id, columns));
+
+        public IEnumerable<T> GetByConditions(IEnumerable<ConditionExpression> conditions, LogicalOperator logicalOperator = LogicalOperator.And, ColumnSet columns = null)
+        {
+            var filter = new FilterExpression(logicalOperator);
+            foreach (var condition in conditions) filter.AddCondition(condition);
+            return GetByFilter(filter, columns);
+        }
+
+        public async Task<IEnumerable<T>> GetByConditionsAsync(IEnumerable<ConditionExpression> conditions, LogicalOperator logicalOperator = LogicalOperator.And, ColumnSet columns = null)
+            => await Task.FromResult(GetByConditions(conditions, logicalOperator, columns));
+
+        public IEnumerable<T> GetByFilter(FilterExpression filter, ColumnSet columns = null, int pageSize = 5000, int pageNumber = 1)
+        {
+            var queryExpression = new QueryExpression(_entityLogicalName)
+            {
+                ColumnSet = columns ?? new ColumnSet(true),
+                Criteria = filter
+            };
+            return RetrieveAll(queryExpression, pageSize, pageNumber);
+        }
+
+        public async Task<IEnumerable<T>> GetByFilterAsync(FilterExpression filter, ColumnSet columns = null, int pageSize = 5000, int pageNumber = 1)
+            => await Task.FromResult(GetByFilter(filter, columns, pageSize, pageNumber));
+
+        public IEnumerable<T> GetByFetchXml(string fetchXml)
+        {
+            var fetchExpression = new FetchExpression(fetchXml);
+            var response = _service.RetrieveMultiple(fetchExpression);
+            return response.Entities.Select(e => e.ToEntity<T>());
+        }
+
+        public async Task<IEnumerable<T>> GetByFetchXmlAsync(string fetchXml)
+            => await Task.FromResult(GetByFetchXml(fetchXml));
+
+        public Guid Create(T entity)
+            => CreateWithRequest(entity);
+
+        public async Task<Guid> CreateAsync(T entity)
+            => await Task.FromResult(Create(entity));
+
+        public void Update(T entity)
+        {
+            UpdateWithRequest(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+            => await Task.Run(() => Update(entity));
+
+        public void Upsert(T entity)
+        {
+            if (entity.Id == Guid.Empty)
+                throw new InvalidOperationException("Entity must have a valid Id for upsert.");
+
+            var request = new UpsertRequest { Target = entity };
+            _service.Execute(request);
+        }
+
+        public async Task UpsertAsync(T entity)
+            => await Task.Run(() => Upsert(entity));
+
+        public void Delete(Guid id)
+        {
+            _service.Delete(_entityLogicalName, id);
+        }
+
+        public async Task DeleteAsync(Guid id)
+            => await Task.Run(() => Delete(id));
+
+        public bool Exists(IEnumerable<ConditionExpression> conditions)
+        {
+            var queryExpression = new QueryExpression(_entityLogicalName)
+            {
+                ColumnSet = new ColumnSet(false),
+                TopCount = 1,
+                Criteria = new FilterExpression()
+            };
+
+            foreach (var condition in conditions)
+                queryExpression.Criteria.AddCondition(condition);
+
+            return RetrieveAll(queryExpression).Any();
+        }
+        public async Task<bool> ExistsAsync(IEnumerable<ConditionExpression> conditions)
+        {
+            return await Task.FromResult(Exists(conditions));
+        }
+
+        public async Task<int> CountAsync(IEnumerable<ConditionExpression> conditions)
+        => await Task.FromResult(Count(conditions));
+
+        public int Count(IEnumerable<ConditionExpression> conditions)
+        {
+            var queryExpression = new QueryExpression(_entityLogicalName)
+            {
+                ColumnSet = new ColumnSet(false),
+                Criteria = new FilterExpression()
+            };
+
+            foreach (var condition in conditions)
+                queryExpression.Criteria.AddCondition(condition);
+
+            return RetrieveAll(queryExpression).Count();
+        }
+
+        public void Associate(Guid sourceId, string relationshipName, EntityReferenceCollection targets)
+            => _service.Associate(_entityLogicalName, sourceId, new Relationship(relationshipName), targets);
+
+        public async Task AssociateAsync(Guid sourceId, string relationshipName, EntityReferenceCollection targets)
+            => await Task.Run(() => Associate(sourceId, relationshipName, targets));
+
+        public void Disassociate(Guid sourceId, string relationshipName, EntityReferenceCollection targets)
+            => _service.Disassociate(_entityLogicalName, sourceId, new Relationship(relationshipName), targets);
+
+        public async Task DisassociateAsync(Guid sourceId, string relationshipName, EntityReferenceCollection targets)
+            => await Task.Run(() => Disassociate(sourceId, relationshipName, targets));
+
+        public OrganizationResponse Execute(OrganizationRequest request)
+            => _service.Execute(request);
+
+        public async Task<OrganizationResponse> ExecuteAsync(OrganizationRequest request)
+            => await Task.FromResult(Execute(request));
+
+
+
+        public Guid CreateWithRequest(T entity, Dictionary<string, object> inputParams = null)
+        {
+            var request = new CreateRequest { Target = entity };
+
+            if (inputParams != null)
+                foreach (var param in inputParams)
+                    request.Parameters[param.Key] = param.Value;
+
+            var response = (CreateResponse)_service.Execute(request);
+            return response.id;
+        }
+
+        public void UpdateWithRequest(T entity, Dictionary<string, object> inputParams = null)
+        {
+            var request = new UpdateRequest { Target = entity };
+
+            if (inputParams != null)
+                foreach (var param in inputParams)
+                    request.Parameters[param.Key] = param.Value;
+
+            _service.Execute(request);
+        }
+
+        public void DeleteWithRequest(Guid id, Dictionary<string, object> inputParams = null)
+        {
+            var request = new DeleteRequest { Target = new EntityReference(_entityLogicalName, id) };
+
+            if (inputParams != null)
+                foreach (var param in inputParams)
+                    request.Parameters[param.Key] = param.Value;
+
+            _service.Execute(request);
+        }
+
+        private IEnumerable<T> RetrieveAll(QueryExpression queryExpression, int pageSize = 5000, int pageNumber = 1)
+        {
+            var results = new List<T>();
+            queryExpression.PageInfo = new PagingInfo { PageNumber = pageNumber, Count = pageSize };
+
+            var hasMoreRecords = true;
+            while (hasMoreRecords)
+            {
+                var response = _service.RetrieveMultiple(queryExpression);
+                results.AddRange(response.Entities.Select(e => e.ToEntity<T>()));
+
+                hasMoreRecords = response.MoreRecords;
+                if (hasMoreRecords)
+                {
+                    queryExpression.PageInfo.PageNumber++;
+                    queryExpression.PageInfo.PagingCookie = response.PagingCookie;
+                }
+            }
+
+            return results;
+        }
+    }
+}
